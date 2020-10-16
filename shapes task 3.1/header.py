6dedabdaf3882973c65773dc38bc78c0a674f25e
karim kohel <kareemkohel@gmail.com>
2020-10-16 23:00:34 +0200

    added 2

---
+++ shapes task 3.1/header.py
@@ -0,0 +1,20 @@
+import cv2
+
+def getShape(approx):
+    
+    pts = len(approx)
+    
+    if pts == 4:
+        (x, y, w, h) = cv2.boundingRect(approx)
+        ratio = w / float(h)
+        shape = "square" if ratio >= 0.90 and ratio <= 1.1 else "rectangle"
+
+    elif pts == 3:
+        shape = "triangle"
+
+    elif 4 < pts < 50:
+        shape = "circle" if pts > 25 else "star"
+    else:
+        shape = "N/A"
+
+    return shape
