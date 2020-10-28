28565baf0400477c9f9f067de6dcd899dbd8ea1c
karim kohel <kareemkohel@gmail.com>
2020-10-28 21:24:01 +0200

    9:23

--- save/save.py
+++ save/save.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 i = 0
 while(cap.isOpened()):
@@ -10,7 +10,7 @@ while(cap.isOpened()):
         i += 1
         cv2.imwrite(f'frame{i}.jpg',frame)
 
-    elif cv2.waitKey(1) == ord('q'):
+    elif cv2.waitKey(20) == ord('q'):
         break
 
     cv2.imshow("Frame", frame)
---
+++ task 3.1/tryosama/colortrig.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+
+
+
+def nothing(x):
+    # any operation
+    pass
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 180, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 66, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 134, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 243, 255, nothing)
+
+font = cv2.FONT_HERSHEY_COMPLEX
+
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L-S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L-V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U-V", "Trackbars")
+
+    lower_red = np.array([l_h, l_s, l_v])
+    upper_red = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel)
+
+    # Contours detection
+    if int(cv2.__version__[0]) > 3:
+        # Opencv 4.x.x
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        # Opencv 3.x.x
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        approx = cv2.approxPolyDP(cnt, 0.025*cv2.arcLength(cnt, True), True)
+        x = approx.ravel()[0]
+        y = approx.ravel()[1]
+
+        if area > 400:
+            cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
+            print(len(approx))
+
+            if len(approx) == 3:
+                shape = "3rd Century BC"
+
+            elif len(approx) == 4:
+                (x, y, w, h) = cv2.boundingRect(approx)
+                ratio = w / float(h)
+                #       square                                                   rectangle
+                shape = "4th Century BC" if ratio >= 0.90 and ratio <= 1.1 else "6th Century BC"
+
+            elif 9 < len(approx) < 15:
+                shape = "5th Century BC"
+
+            else:
+                shape = "2nd Century BC"
+
+            cv2.putText(frame, shape, (x, y), font, 1, (0, 0, 0))
+
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1)
+
+
+
+
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
